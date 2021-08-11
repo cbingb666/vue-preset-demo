49976848c775bcf55c0ee7765c2e753d997f6049
蔡柄泓 <caibinghong@tuzhanai.com>
2021-08-11 23:46:41 +0800

    feat(template/base): 添加pacakge.json script commit:all

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,11 @@
+const TEMPLATES_PATH = '../template'
+
 module.exports = api => {
+  api.render(TEMPLATES_PATH + '/base');
   api.extendPackage({
     "scripts": {
       "commit": "cz",
+      "commit:all": "git add . && cz",
     }
   })
 }
